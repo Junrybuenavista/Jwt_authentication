d042bc0530220cf74ec59216446cc5a95e1f4f15
Junrybuenavista <42229618+Junrybuenavista@users.noreply.github.com>
2024-06-19 10:14:18 +0800

    aa

--- helper/test.js
+++ helper/test.js
@@ -1,32 +1,14 @@
-const { error } = require("@hapi/joi/lib/base")
 
 
 
-async function fun1(){
-    //console.log('func1')
-    throw 'kk'
-    //throw 'error ko'
+
+function fac(n){
+    console.log('d')
+    if(n===0){
+        return 1
+    }
+    else{
+        return n * fac(n - 1)
+    }
 }
-
-async function fun2(stIn){ 
-    return 'return1'+stIn   
-}
-
-
-
-
-fun2('hhehehe').then(temp=>{   
-    
-    console.log(temp)
-
-    fun1().then(()=>{
-        console.log('hh')
-    }).catch(error=>{
-        console.log('akong error'+error)
-    })
-   
-}).catch(error=>{
-    console.log(error)
-})
-
-
+console.log(fac(-1))
